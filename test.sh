6a9961f2f7119c24cfa9d78e3e4de8d34e9acba3
vganesh008 <ganeshgane008@gmail.com>
2021-05-23 20:40:46 +0530

    added last line of code End

--- test.sh
+++ test.sh
@@ -2,27 +2,24 @@
 #################################################################
 #Created by:=Ganesh Reddy                                       #
 #Date on created=
-echo "created date=`date`
+echo "created date=$(date)
 #Date on Modified =
-echo " modified date =`date`
+echo " modified date =$(date)
 
 echo "Creating Test script"
 ================================================================
 echo "Provide File name"
 read -r file
-if [ -z $file ];
-then
-echo "provide valied input"
+if [ -z $file ]; then
+    echo "provide valied input"
 fi
 
 cd test/
 
-if [ -f /root/test/$file ];
-then
-echo "file is avilable on path"
-else 
-echo "let me create new file"
-touch $file.sh
+if [ -f /root/test/$file ]; then
+    echo "file is avilable on path"
+else
+    echo "let me create new file"
+    touch $file.sh
 fi
-
-
+echo "code was done"
